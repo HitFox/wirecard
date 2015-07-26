7a3a692cd07f22a8f1931100663d559a090dd263
Dominic Breuker <dominic.breuker@gmail.com>
2015-07-26 20:31:17 +0200

    refactor response parsing

--- lib/wirecard/response.rb
+++ lib/wirecard/response.rb
@@ -1,13 +1,13 @@
 module Wirecard
   class Response
+    attr_reader :body
     
     def initialize(http_response)
-      @header = http_response.header
       @body = http_response.body
     end
     
     def to_hash
-      Hash[CGI.parse(@body).map{ |param| extract_key_value(param) }]
+      Hash[CGI.parse(body).map{ |param| extract_key_value(param) }]
     end
     
     ### ------------------------------------------ ###
