d2e3e8a63f70614b75bca54805527bcf2db5ec67
Dominic Breuker <dominic.breuker@gmail.com>
2015-11-03 15:16:15 +0100

    try fixing specs on circleci - working locally

--- spec/unit_tests/payment_process/init_spec.rb
+++ spec/unit_tests/payment_process/init_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe Wirecard::PaymentProcess::Init do
-  
+
   let(:init) { Wirecard::PaymentProcess::Init.new(options) }
   let(:options) { Hash.new.merge(payment_type_data).merge(payment_data).merge(consumer_data) }
   let(:payment_type_data) { { payment_type: 'CCARD', storage_id: 'b2737b746627482e0b024097cadb1b41' } }
   let(:payment_data) { { amount: '1000', currency: 'USD', order_description: 'some description' } }
   let(:consumer_data) { { consumer_ip_address: '127.0.0.1', consumer_user_agent: 'some agent', language: 'en'} }
-  
+
   include_context 'configuration'
   include_context 'stub requests'
-  
+
   it { is_expected.to be_a_kind_of(Wirecard::PaymentProcess::Init) }
-  
+
   describe '#implicit_fingerprint_order' do
     subject { init.implicit_fingerprint_order }
 
@@ -32,22 +32,22 @@ RSpec.describe Wirecard::PaymentProcess::Init do
     it { expect(subject[:service_url]).to eq(config[:service_url]) }
     it { expect(subject[:confirm_url]).to eq(config[:confirm_url]) }
   end
-  
+
   describe '#url' do
     subject { init.url }
-    
+
     it { is_expected.to eq('https://checkout.wirecard.com/seamless/frontend/init') }
   end
-  
+
   describe '#post' do
     subject { init.post.response }
 
     context 'when credit card payment' do
       let(:payment_type_data) { { payment_type: 'CCARD', storage_id: 'b2737b746627482e0b024097cadb1b41' } }
-      
+
       it { is_expected.to eq ({redirect_url: "https://checkout.wirecard.com/seamless/frontend/D200001qmore_DESKTOP/select.php?SID=ttkbc64otkqk067oca49ft0cr5" } ) }
     end
-    
+
     context 'when Sofortüberweisung payment' do
       let(:payment_type_data) { { payment_type: 'SOFORTUEBERWEISUNG' } }
 
@@ -67,7 +67,8 @@ RSpec.describe Wirecard::PaymentProcess::Init do
     end
 
     context 'when options are invalid' do
-      let(:options) { Hash.new }
+      subject { Wirecard::PaymentProcess::Init.new(Hash.new).post.response }
+      #let(:options) { Hash.new }
 
       it { is_expected.to eq ({
          :"error.1.consumer_message" => "PAYMENTTYPE is missing.",
