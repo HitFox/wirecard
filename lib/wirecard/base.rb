86444e9ab2709a5d1543d40eff8e43c922d83dc7
Dominic Breuker <dominic.breuker@gmail.com>
2015-11-04 12:45:09 +0100

    refactor specs and fix error on circleci

--- lib/wirecard/base.rb
+++ lib/wirecard/base.rb
@@ -1,17 +1,17 @@
 module Wirecard
   class Base
-    
+
     attr_reader :request
-    
+
     attr_accessor :response, :params
-    
+
     def defaults
       @defaults ||= {
         customer_id: Wirecard.config.customer_id,
         shop_id: Wirecard.config.shop_id
       }
     end
-    
+
     def initialize(params = {})
       @request = Wirecard::Request.new(
         params: defaults.merge(params),
@@ -19,17 +19,17 @@ module Wirecard
         uri: uri
       )
     end
-    
+
     def post
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
-      
+
       self.response = Wirecard::Response.new(http.request(request.to_post)).to_hash
       self
     end
-    
+
     alias_method :save, :post
-    
+
     def method_missing(method_name, *args, &block)
       if response && (response.key?(method_name.to_sym) || response.key?(method_name))
         response[method_name.to_sym] || response[method_name]
@@ -37,30 +37,30 @@ module Wirecard
         super
       end
     end
-    
+
     def respond_to_missing?(method_name, include_private = false)
       response && (response.key?(method_name.to_sym) || response.key?(method_name)) || super
     end
-    
+
     def implicit_fingerprint_order
       nil
     end
-    
+
     def self.create(attributes = {})
       object = new(attributes)
       object.save
       object
     end
-    
+
     private
-    
+
     def uri
       @uri ||= URI.parse(url)
     end
-    
+
     def url
       raise NotImplementedError, 'A URL must be given to make a call'
     end
-    
+
   end
 end
